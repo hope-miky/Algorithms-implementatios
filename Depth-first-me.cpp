1c5b3734029414f7d6923f481d17e9017be22c32
Tesfamichael1074 <hope.miky1074@gmail.com>
2019-10-17 08:27:15 +0300

    Graph updated

--- Depth-first-me.cpp
+++ Depth-first-me.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#define maxv 10
 
 using namespace std;
 
@@ -8,6 +9,35 @@ struct vertex{
 };
 
 
+int stack[maxv];
+int edges[maxv][maxv];
+int vertexcounter = 0;
+int top = -1;
+
+struct vertex* vertexlist[maxv];
+
+// Stack methos 
+void push(int val){
+    stack[++top];
+}
+
+int peek(){
+    return stack[top];
+}
+
+void pop(){
+    top--;
+}
+
+
+// Graph methods 
+
+void addVertex(int label){
+    struct vertex* item = (struct vertex*) malloc(sizeof(struct verttex));
+    item->label = label;
+    item->visited = false;
+    vertexlist[vertexcounter++] = item;
+}
 
 int main(){
 
