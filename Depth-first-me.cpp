2110b1c552d0a9239c57df59f9de6cccb1211fd8
Tesfamichael1074 <hope.miky1074@gmail.com>
2019-10-18 10:15:04 +0300

    Commented Depth first search

--- Depth-first-me.cpp
+++ Depth-first-me.cpp
@@ -3,39 +3,44 @@
 
 using namespace std;
 
+// Defining the node using struct
 struct vertex{
-    char label;
-    bool visited;
+    char label;                                                     // The variable to hold the data
+    bool visited;                                                   //  Variable to show whether the node is visited or not
 };
 
 
-int stack[maxv];
-int edges[maxv][maxv];
-int vertexcounter = 0;
-int top = -1;
+int stack[maxv];                                                    // Stack array to hold the path
+int edges[maxv][maxv];                                              // Array to the edges 
+int vertexcounter = 0;                                              // To show how many vertexes are there
+int top = -1;                                                           
 
-struct vertex* vertexlist[maxv];
+struct vertex* vertexlist[maxv];                                    // Pointer array of pointers holding the struct
 
 // Stack methos 
+//Push 
 void push(int val){
     stack[++top] = val;
 }
 
+// Peek the top of the stack
 int peek(){
     return stack[top];
 }
 
+//Pop
 void pop(){
     top--;
 }
 
+// Check if the stack is empty
 bool isEmpty(){
     return top == -1;
 }
 
 
 // Graph methods 
-
+// Add a vertex to the array
 void addVertex(char label){
     struct vertex* item = (struct vertex*) malloc(sizeof(struct verttex*));
     item->label = label;
@@ -43,11 +48,13 @@ void addVertex(char label){
     vertexlist[vertexcounter++] = item;
 }
 
+// Add edge from start to end and viseversal
 void addEdge(int start, int end){
     edges[start][end] = 1;
     edges[end][start] = 1;
 }
 
+// Find the next available unvisited node or vertex
 int findNextVertex(int startPos){
     for (int i =0; i<vertexcounter; i++){
         if (edges[startPos][i]==1 && vertexlist[i]->visited==false){
@@ -56,16 +63,20 @@ int findNextVertex(int startPos){
     }
     return -1;
 }
+
+// Display a certain vertex
 void displayVertex(int i){
     cout<<vertexlist[i]->label<<", ";
 }
 
+// Mark all nodes unvisited
 void resetVisit(){
     for (int i=0; i < vertexcounter; i++){
         vertexlist[i]->visited = false;
     }
 }
 
+// Give 0 to all the values in the edges array
 void populate(){
     for (int i=0; i< maxv; i++){
         for (int j=0; j<maxv; j++){
@@ -74,60 +85,65 @@ void populate(){
     }
 }
 
+
+// The main Depth first algorithm
 void depthFirstSearch(){
     int d = 0;
-    vertexlist[d]->visited = true;
-    displayVertex(d);
+    vertexlist[d]->visited = true;                                          // Mark the first edge visited
+    displayVertex(d);                                                       // Display the first node
 
-    push(d);
+    push(d);                                                                // Push the first node to a stack
 
-    
+    while(!isEmpty()){                                                      
 
-    while(!isEmpty()){
+        int unvisted = findNextVertex(peek());                              // Grap the next unvisited node
 
-        int unvisted = findNextVertex(peek());
-
-        if(unvisted == -1){
+        if(unvisted == -1){                                                 // If there is no unvisted node pop from the stack
             pop();
         }
         else{
-            displayVertex(unvisted);
-            vertexlist[unvisted]->visited = true;
-            push(unvisted);
+            displayVertex(unvisted);                                        // Display the node data
+            vertexlist[unvisted]->visited = true;                           // Mark is visited
+            push(unvisted);                                                 // Push it to the stack
             
         }
     }
 
-    resetVisit();
+    resetVisit();                                                           // Reset the nodes
 }
 
 
 
 int main(){
-
+    // Remove all edges
     populate();
 
+    // Add 'A' Node
     addVertex('A');
 
+    // Add 'B' Node
     addVertex('B');
 
+    // Add 'C' Node
     addVertex('C');
 
+    // Add 'D' Node
     addVertex('D');
 
+    // Add 'E' Node
     addVertex('E');
 
 
-    //addEdge(0, 1);
-    //addEdge(0, 3);
-    addEdge(0, 4);
-    addEdge(1, 2);
-    addEdge(2, 3);
-    addEdge(2, 4);
-    addEdge(4, 3);
+    //addEdge(0, 1);                // Connect  A  and  B
+    //addEdge(0, 3);                // Connect  A  and  D
+    addEdge(0, 4);                  // Connect  A  and  E
+    addEdge(1, 2);                  // Connect  B  and  C
+    addEdge(2, 3);                  // Connect  C  and  D
+    addEdge(2, 4);                  // Connect  C  and  E
+    addEdge(4, 3);                  // Connect  D  and  E
 
 
-    depthFirstSearch();
+    depthFirstSearch();             // Start the travers algorithm
 
 
     return 0;
